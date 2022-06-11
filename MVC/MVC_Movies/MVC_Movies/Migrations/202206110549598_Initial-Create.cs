95601b7f2e9bab1d5e6de789c7581a99001e1364
vajahullasharif <vajahullamohmmed@gmail.com>
2022-06-11 11:41:41 +0530

    MVCAssignment

---
+++ MVC/MVC_Movies/MVC_Movies/Migrations/202206110549598_Initial-Create.cs
@@ -0,0 +1,27 @@
+﻿namespace MVC_Movies.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.TblMovie",
+                c => new
+                    {
+                        MovieId = c.Int(nullable: false, identity: true),
+                        MovieName = c.String(),
+                        ReleaseDate = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.MovieId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.TblMovie");
+        }
+    }
+}
---
+++ MVC/MVC_Movies/MVC_Movies/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Movies.Models
+{
+    [Table("TblMovie")]
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+        public Nullable<System.DateTime> ReleaseDate { get; set; }
+    }
+}
