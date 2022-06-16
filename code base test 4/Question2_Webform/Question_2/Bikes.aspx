98162956fef37a932e2894bd792479928c9d9e3b
vajahullasharif <vajahullamohmmed@gmail.com>
2022-06-16 18:02:36 +0530

    Codebasetest4

---
+++ code base test 4/Question2_Webform/Question_2/Bikes.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Bikes.aspx.cs" Inherits="Question_2.Bikes" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+<div>
+</div>
+Select Any Bike: <asp:DropDownList ID="DropDownList1" runat="server" Height="25px" Width="170px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+</asp:DropDownList>
+<br/>
+<asp:Image ID="Image1" runat="server" Height="300px" style="margin-left: 30px; margin-top: 40px" Width="480px" ImageUrl="~/Images/Hayabusa bike.jfif" />
+<p>
+The Price of this Bike : <asp:Button ID="Price" runat="server" OnClick="Button1_Click1" style="margin-left: 40px" Text="Price" Width="119px" />
+</p>
+<p>
+<asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged1" style="margin-top: 0px" Width="140px"></asp:TextBox>
+</p>
+</form>
+</body>
+</html>
---
+++ code base test 4/Question2_Webform/Question_2/Bikes.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Question_2
+{
+    public partial class Bikes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] str = new string[] { "select", "Hayabusa bike", "Bmw bike", "Harley Davidson bike", "KTM bike", "R15 bike" };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+
+        }
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/Images/" + str + ".jfif";
+
+        }
+        protected void TextBox1_TextChanged1(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            TextBox1.Text = DropDownList1.SelectedIndex.ToString();
+            if (DropDownList1.Text == "Hayabusa bike")
+            {
+                TextBox1.Text = "Rs 1600000";
+            }
+            else if (DropDownList1.Text == "Bmw bike")
+            {
+                TextBox1.Text = "Rs 1400000";
+            }
+            else if (DropDownList1.Text == "Harley Davidson bike")
+            {
+                TextBox1.Text = "Rs 1500000";
+            }
+            else if (DropDownList1.Text == "KTM bike")
+            {
+                TextBox1.Text = "Rs 1600000";
+            }
+            else if (DropDownList1.Text == "R15 bike")
+            {
+                TextBox1.Text = "Rs 1200000";
+            }
+            else
+            {
+                TextBox1.Text = "Rs 4000000";
+            }
+        }
+    }
+}
